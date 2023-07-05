314dad67071da71756012a33136ed7058ae5afbb
parin271194 <138728623+parin271194@users.noreply.github.com>
2023-07-05 18:07:13 -0400

    Hello Azure

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,12 +9,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@RequestMapping("/")
-	String sayHello() {
-		return "Hello World!";
-	}
+    @RequestMapping("/")
+    String home() {
+        return "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <title>Basic Website</title>\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: Arial, sans-serif;\n" +
+                "            margin: 0;\n" +
+                "            padding: 20px;\n" +
+                "            text-align: center;\n" +
+                "        }\n" +
+                "        h1 {\n" +
+                "            color: #333;\n" +
+                "        }\n" +
+                "        input[type=text] {\n" +
+                "            padding: 10px;\n" +
+                "            margin-bottom: 10px;\n" +
+                "        }\n" +
+                "        button {\n" +
+                "            padding: 10px 20px;\n" +
+                "            background-color: #007bff;\n" +
+                "            color: #fff;\n" +
+                "            border: none;\n" +
+                "            cursor: pointer;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "    <script>\n" +
+                "        function handleClick() {\n" +
+                "            var inputText = document.getElementById('myInput').value;\n" +
+                "            alert('You entered: ' + inputText);\n" +
+                "        }\n" +
+                "        document.getElementById('myButton').addEventListener('click', handleClick);\n" +
+                "    </script>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <h1>My Basic Website</h1>\n" +
+                "    <label for='myInput'>Enter some text:</label>\n" +
+                "    <input type='text' id='myInput'>\n" +
+                "    <br>\n" +
+                "    <button id='myButton'>Click Me</button>\n" +
+                "</body>\n" +
+                "</html>";
+    }
 }
