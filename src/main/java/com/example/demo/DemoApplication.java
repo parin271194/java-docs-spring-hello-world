4a678416cff62931e7cfbe5a173c5e471164791c
parin271194 <138728623+parin271194@users.noreply.github.com>
2023-07-06 00:29:15 -0400

    commit6

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -25,6 +25,7 @@ public class DemoApplication {
     public String about(Model model) {
         model.addAttribute("pageTitle", "About");
         model.addAttribute("aboutText", "We are a passionate team dedicated to providing excellent services.");
+```java
         return "about";
     }
 
@@ -36,77 +37,3 @@ public class DemoApplication {
         return "contact";
     }
 }
-<!DOCTYPE html>
-<html xmlns:th="http://www.thymeleaf.org">
-<head>
-    <title th:text="${pageTitle}"></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            padding: 20px;
-        }
-        h1 {
-            color: #333;
-        }
-        nav {
-            background-color: #007bff;
-            padding: 10px;
-            margin-bottom: 20px;
-        }
-        nav a {
-            color: #fff;
-            text-decoration: none;
-            margin-right: 10px;
-        }
-        .container {
-            margin-top: 20px;
-        }
-    </style>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const navLinks = document.querySelectorAll('.nav-link');
-            const sections = document.querySelectorAll('section');
-            
-            navLinks.forEach(function(link) {
-                link.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    const targetSection = document.querySelector(link.getAttribute('href'));
-                    window.scrollTo({
-                        top: targetSection.offsetTop,
-                        behavior: 'smooth'
-                    });
-                });
-            });
-        });
-    </script>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">My Website</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link"> (Continuation of the corrected code)
-
-```html
- href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <div class="container">
-        <h1 th:text="${pageTitle}"></h1>
-        <p th:text="${welcomeMessage}"></p>
-    </div>
-</body>
-</html>
