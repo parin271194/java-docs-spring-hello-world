41db5b1ac9fb5a351d3cc88ffaa4e48aa1f37b40
parin271194 <138728623+parin271194@users.noreply.github.com>
2023-07-06 00:00:11 -0400

    commit3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,83 +2,119 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @RequestMapping("/")
-    String home() {
-        return "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "    <title>Advanced Website</title>\n" +
-                "    <style>\n" +
-                "        body {\n" +
-                "            font-family: Arial, sans-serif;\n" +
-                "            margin: 0;\n" +
-                "            padding: 20px;\n" +
-                "            text-align: center;\n" +
-                "        }\n" +
-                "        h1 {\n" +
-                "            color: #333;\n" +
-                "        }\n" +
-                "        nav {\n" +
-                "            background-color: #007bff;\n" +
-                "            padding: 10px;\n" +
-                "        }\n" +
-                "        nav a {\n" +
-                "            color: #fff;\n" +
-                "            text-decoration: none;\n" +
-                "            margin-right: 10px;\n" +
-                "        }\n" +
-                "        .content {\n" +
-                "            margin-top: 20px;\n" +
-                "        }\n" +
-                "        .hidden {\n" +
-                "            display: none;\n" +
-                "        }\n" +
-                "    </style>\n" +
-                "    <script>\n" +
-                "        function showPage(pageId) {\n" +
-                "            var pages = document.getElementsByClassName('page');\n" +
-                "            for (var i = 0; i < pages.length; i++) {\n" +
-                "                pages[i].classList.add('hidden');\n" +
-                "            }\n" +
-                "            document.getElementById(pageId).classList.remove('hidden');\n" +
-                "        }\n" +
-                "    </script>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "    <nav>\n" +
-                "        <a href='#' onclick=\"showPage('home');\">Home</a>\n" +
-                "        <a href='#' onclick=\"showPage('about');\">About</a>\n" +
-                "        <a href='#' onclick=\"showPage('contact');\">Contact</a>\n" +
-                "    </nav>\n" +
-                "    <div class='content'>\n" +
-                "        <div id='home' class='page'>\n" +
-                "            <h1>Welcome to the Home Page</h1>\n" +
-                "            <p>This is the home page content.</p>\n" +
-                "        </div>\n" +
-                "        <div id='about' class='page hidden'>\n" +
-                "            <h1>About Us</h1>\n" +
-                "            <p>This is the about page content.</p>\n" +
-                "        </div>\n" +
-                "        <div id='contact' class='page hidden'>\n" +
-                "            <h1>Contact Us</h1>\n" +
-                "            <p>This is the contact page content.</p>\n" +
-                "        </div>\n" +
-                "    </div>\n" +
-                "    <script>\n" +
-                "        showPage('home');\n" +
-                "    </script>\n" +
-                "</body>\n" +
-                "</html>";
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("pageTitle", "Home");
+        model.addAttribute("welcomeMessage", "Welcome to our website!");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("pageTitle", "About");
+        model.addAttribute("aboutText", "We are a passionate team dedicated to providing excellent services.");
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("pageTitle", "Contact");
+        model.addAttribute("email", "contact@example.com");
+        model.addAttribute("phone", "+1234567890");
+        return "contact";
     }
 }
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head>
+    <title th:text="${pageTitle}"></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            padding: 20px;
+        }
+        h1 {
+            color: #333;
+        }
+        nav {
+            background-color: #007bff;
+            padding: 10px;
+            margin-bottom: 20px;
+        }
+        nav a {
+            color: #fff;
+            text-decoration: none;
+            margin-right: 10px;
+        }
+        .container {
+            margin-top: 20px;
+        }
+    </style>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const navLinks = document.querySelectorAll('.nav-link');
+            const sections = document.querySelectorAll('section');
+            
+            navLinks.forEach(function(link) {
+                link.addEventListener('click', function(event) {
+                    event.preventDefault();
+                    const targetSection = document.querySelector(link.getAttribute('href'));
+                    window.scrollTo({
+                        top: targetSection.offsetTop,
+                        behavior: 'smooth'
+                    });
+                });
+            });
+        });
+    </script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">My Website</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact">Contact> (Continuation of the `home.html` template)
+
+```html
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <h1 th:text="${pageTitle}"></h1>
+        <p th:text="${welcomeMessage}"></p>
+    </div>
+</body>
+</html>
+<div class="container">
+    <h1 th:text="${pageTitle}"></h1>
+    <p th:text="${aboutText}"></p>
+</div>
+<div class="container">
+    <h1 th:text="${pageTitle}"></h1>
+    <p>Email: <span th:text="${email}"></span></p>
+    <p>Phone: <span th:text="${phone}"></span></p>
+</div>
